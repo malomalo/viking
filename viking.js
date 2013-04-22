be620b39a8b7cb160cc79c518e2db50b9ca27623
Jon Bracy <jonbracy@gmail.com>
2013-04-21 22:28:28 -0700

    Started Viking.Collection documentation

--- viking.js
+++ viking.js
@@ -191,8 +191,8 @@ Viking.config = function (obj, key, val) {
 // ------------
 //
 // Viking.Model is an extension of [Backbone.Model](http://backbonejs.org/#Model).
-// It adds naming,  relationships, data type coerions, and modifies sync to
-// work with [Ruby on Rails](http://rubyonrails.org/) out of the box.
+// It adds naming, relationships, data type coerions, selection, and modifies
+// sync to work with [Ruby on Rails](http://rubyonrails.org/) out of the box.
 Viking.Model = Backbone.Model.extend({
     
     constructor: function () {
@@ -289,6 +289,127 @@ Viking.Model = Backbone.Model.extend({
     // TODO
     find: function(id, options) {
         Backbone.sync('GET', new this({id: id}), options);
+    }
+    
+});
+// Viking.Collection
+// -----------------
+//
+// Viking.Collection is an extension of [Backbone.Collection](http://backbonejs.org/#Collection).
+// It adds predicates, selection, and modifies fetch to cancel any current
+// request if a new fetch is triggered.
+Viking.Collection = Backbone.Collection.extend({
+    
+    // Set the default model to a generic Viking.Model
+    model: Viking.Model,
+
+    constructor: function(models, options) {
+        Backbone.Collection.apply(this, arguments);
+        
+        if(options && options.predicate) {
+            this.setPredicate(options.predicate, {silent: true});
+        }
+    },
+    
+    url: function() {
+        return "/" + this.model.modelName.underscore().pluralize();
+    },
+    paramRoot: function() {
+        return this.model.modelName.underscore().pluralize();
+    },
+    
+    // If a predicate is set it's paramaters will be passed under the
+    // predicate namespace when querying the server. #predicateChanged is
+    // set as a callback on the `change` event for the predicate
+    //
+    // #setPredicate accepts either attributes to instaniate a
+    // Viking.Predicate or an instanceof a Viking.Predicate
+    //
+    // To remove a predeicate call `#setPredicate` with a falsey value.
+    //
+    // Calling #setPredicate and setting it the same object that is currently
+    // the predicate will not trigger a #predicateChanged call
+    setPredicate: function(predicate, options) {
+        if (this.predicate === predicate) { return false; }
+        
+        if(this.predicate) { this.stopListening(this.predicate); }
+        
+        if(predicate) {
+            if(!(predicate instanceof Viking.Predicate)) {
+                predicate = new Viking.Predicate(predicate);
+            }
+            this.predicate = predicate;
+            this.listenTo(predicate, 'change', this.predicateChanged);
+            if(!(options && options.silent)) { this.predicateChanged(); }
+        } else if (this.predicate) {
+            delete this.predicate;
+            if(!(options && options.silent)) { this.predicateChanged(); }
+        }
+    },
+    
+    // Called when the predicate is changed. Having this being called
+    // when the predicate changes instead of just `fetch` allows sub
+    // collections to overwrite what happens when it changes. An example
+    // of this would be the `Viking.PaginatedCollection`
+    predicateChanged: function(predicate, options) {
+        this.fetch();
+    },
+
+    // Sets `'selected'` to `true` on the `model`. If `clearCurrentlySelected`
+    // is truthy all other models will have `selected` set to `false`.
+    // Also triggers the `selected` event on the collection. If the model is
+    // already selected the `selected` event is not triggered
+    select: function(model, clearCurrentlySelected) {
+        if(!clearCurrentlySelected) {
+            this.clearSelected(model);
+        }
+        if(!model.get('selected')) {
+            model.set('selected', true);
+            this.trigger('selected', this.selected());
+        }
+    },
+    
+    // returns all the models where `selected` == true
+    selected: function() {
+        return this.filter(function(m) { return m.get('selected'); });
+    },
+    
+    // Sets `'selected'` to `false` on all models
+    clearSelected: function(exceptModel) {
+        if(exceptModel instanceof Viking.Model) {
+            exceptModel = exceptModel.cid;
+        }
+        this.each(function(m) {
+            if(m.cid !== exceptModel) {
+                m.set('selected', false);
+            }
+        });
+    },
+    
+    // Override the default Backbone.Collection#fetch to cancel any current
+    // fetch request if fetch is called again. For example when the predicate
+    // changes 3 times, if the first 2 request don't return before the 3rd is
+    // sent they will be canceled and only the last one will finish and update
+    // the collection. You won't get the collection being updated 3 times.
+    fetch: function(options) {
+        options || (options = {});
+        
+        var complete = options.complete;
+        options.complete = _.bind(function() {
+            delete this.xhr;
+            if(complete) { complete(); }
+        }, this);
+        
+        if (this.xhr) { this.xhr.abort(); }
+        this.xhr = Backbone.Collection.prototype.fetch.call(this, options);
+    },
+    
+    sync: function(method, model, options) {
+        if(method === 'read' && this.predicate) {
+            options.data || (options.data = {});
+            options.data.predicate = this.predicate.attributes;
+        }
+        return Backbone.sync.call(this, method, model, options);
     }
     
 });
@@ -433,119 +554,6 @@ Backbone.Model.prototype.toJSON = function (options) {
 
     return data;
 };
-Viking.Collection = Backbone.Collection.extend({
-    model: Viking.Model,
-
-    constructor: function(models, options) {
-        Backbone.Collection.apply(this, arguments);
-        
-        if(options && options.predicate) {
-            this.setPredicate(options.predicate, {silent: true});
-        }
-    },
-    
-    url: function() {
-        return "/" + this.model.modelName.underscore().pluralize();
-    },
-    paramRoot: function() {
-        return this.model.modelName.underscore().pluralize();
-    },
-    
-    // If a predicate is set it's paramaters will be passed under the
-    // predicate namespace when querying the server. #predicateChanged is
-    // set as a callback on the `change` event for the predicate
-    //
-    // #setPredicate accepts either attributes to instaniate a
-    // Viking.Predicate or an instanceof a Viking.Predicate
-    //
-    // To remove a predeicate call `#setPredicate` with a falsey value.
-    //
-    // Calling #setPredicate and setting it the same object that is currently
-    // the predicate will not trigger a #predicateChanged call
-    setPredicate: function(predicate, options) {
-        if (this.predicate === predicate) { return false; }
-        
-        if(this.predicate) { this.stopListening(this.predicate); }
-        
-        if(predicate) {
-            if(!(predicate instanceof Viking.Predicate)) {
-                predicate = new Viking.Predicate(predicate);
-            }
-            this.predicate = predicate;
-            this.listenTo(predicate, 'change', this.predicateChanged);
-            if(!(options && options.silent)) { this.predicateChanged(); }
-        } else if (this.predicate) {
-            delete this.predicate;
-            if(!(options && options.silent)) { this.predicateChanged(); }
-        }
-    },
-    
-    // Called when the predicate is changed. Having this being called
-    // when the predicate changes instead of just `fetch` allows sub
-    // collections to overwrite what happens when it changes. An example
-    // of this would be the `Viking.PaginatedCollection`
-    predicateChanged: function(predicate, options) {
-        this.fetch();
-    },
-
-    // Sets `'selected'` to `true` on the `model`. If `clearCurrentlySelected`
-    // is truthy all other models will have `selected` set to `false`.
-    // Also triggers the `selected` event on the collection. If the model is
-    // already selected the `selected` event is not triggered
-    select: function(model, clearCurrentlySelected) {
-        if(!clearCurrentlySelected) {
-            this.clearSelected(model);
-        }
-        if(!model.get('selected')) {
-            model.set('selected', true);
-            this.trigger('selected', this.selected());
-        }
-    },
-    
-    // returns all the models where `selected` == true
-    selected: function() {
-        return this.filter(function(m) { return m.get('selected'); });
-    },
-    
-    // Sets `'selected'` to `false` on all models
-    clearSelected: function(exceptModel) {
-        if(exceptModel instanceof Viking.Model) {
-            exceptModel = exceptModel.cid;
-        }
-        this.each(function(m) {
-            if(m.cid !== exceptModel) {
-                m.set('selected', false);
-            }
-        });
-    },
-    
-    // Override the default Backbone.Collection#fetch to cancel any current
-    // fetch request if fetch is called again. For example when the predicate
-    // changes 3 times, if the first 2 request don't return before the 3rd is
-    // sent they will be canceled and only the last one will finish and update
-    // the collection. You won't get the collection being updated 3 times.
-    fetch: function(options) {
-        options || (options = {});
-        
-        var complete = options.complete;
-        options.complete = _.bind(function() {
-            delete this.xhr;
-            if(complete) { complete(); }
-        }, this);
-        
-        if (this.xhr) { this.xhr.abort(); }
-        this.xhr = Backbone.Collection.prototype.fetch.call(this, options);
-    },
-    
-    sync: function(method, model, options) {
-        if(method === 'read' && this.predicate) {
-            options.data || (options.data = {});
-            options.data.predicate = this.predicate.attributes;
-        }
-        return Backbone.sync.call(this, method, model, options);
-    }
-    
-});
 Viking.PaginatedCollection = Viking.Collection.extend({
     constructor: function(models, options) {
         Viking.Collection.apply(this, arguments);
@@ -683,8 +691,8 @@ Viking.Router = Backbone.Router.extend({
 
 
 
-//
 
+//
 
 
 
