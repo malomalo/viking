9a9bb080335886fdc460e733c229dc72c9f5fc79
James R. Bracy <waratuman@gmail.com>
2013-06-18 12:23:50 -0400

    Delaying the loading of router callbacks functions that are determined from strings until the router is called.

--- test/viking/router.js
+++ test/viking/router.js
@@ -1,147 +1,167 @@
-module("Viking.Router");
+(function () {
+    module("Viking.Router");
 
-Viking.Router.prototype.cleanup = function() {
-    this.stop()
-    Backbone.history.handlers = [];
-}
+    Viking.Router.prototype.cleanup = function() {
+        this.stop()
+        Backbone.history.handlers = [];
+    }
 
-test("routes to a controller and action", function() {
-    expect(2);
-    
-    Controller = { action: function() { ok(true); } }
-    var router = Viking.Router.extend({
-        routes: {
-            '': {to: 'Controller#action', name: 'root'}
-        }
+    test("routes to a controller and action", function() {
+        expect(2);
+
+        Controller = { action: function() { ok(true); } }
+        var router = Viking.Router.extend({
+            routes: {
+                '': {to: 'Controller#action', name: 'root'}
+            }
+        });
+        router = new router();
+
+        ok(Backbone.history.handlers[0].route.test(''));
+        Backbone.history.handlers[0].callback('');
+        router.cleanup();
+        delete Controller;
     });
-    router = new router();
-    
-    ok(Backbone.history.handlers[0].route.test(''));
-    Backbone.history.handlers[0].callback('');
-    router.cleanup();
-    delete Controller;
-});
 
-test("routes to a function", function() {
-    expect(2);
-    
-    var func = function() { ok(true); };
-    var router = Viking.Router.extend({
-        routes: {
-            '': func
-        }
+    test("routes to a function", function() {
+        expect(2);
+
+        var func = function() { ok(true); };
+        var router = Viking.Router.extend({
+            routes: {
+                '': func
+            }
+        });
+        router = new router();
+
+        ok(Backbone.history.handlers[0].route.test(''));
+        Backbone.history.handlers[0].callback('');
+        router.cleanup();
+        delete Controller;
     });
-    router = new router();
-    
-    ok(Backbone.history.handlers[0].route.test(''));
-    Backbone.history.handlers[0].callback('');
-    router.cleanup();
-    delete Controller;
-});
 
-test("routes to a function in the router", function() {
-    expect(2);
-    
-    var router = Viking.Router.extend({
-        routes: {
-            '': 'func'
-        },
-        func: function() { ok(true); }
+    test("routes to a function in the router", function() {
+        expect(2);
+
+        var router = Viking.Router.extend({
+            routes: {
+                '': 'func'
+            },
+            func: function() { ok(true); }
+        });
+        router = new router();
+
+        ok(Backbone.history.handlers[0].route.test(''));
+        Backbone.history.handlers[0].callback('');
+        router.cleanup();
+        delete Controller;
     });
-    router = new router();
-    
-    ok(Backbone.history.handlers[0].route.test(''));
-    Backbone.history.handlers[0].callback('');
-    router.cleanup();
-    delete Controller;
-});
 
-test("routes with a regex", function() {
-    expect(5);
-    
-    var router = Viking.Router.extend({
-        routes: {
-            'r/^([a-z][a-z])\/([^\/]+)\/([^\/]+)$/': 'func',
-        },
-        func: function(state,something,another) {
-            equal(state, 'ca');
-            equal(something, 'something');
-            equal(another, 'another');
-        }
+    test("routes with a regex", function() {
+        expect(5);
+
+        var router = Viking.Router.extend({
+            routes: {
+                'r/^([a-z][a-z])\/([^\/]+)\/([^\/]+)$/': 'func',
+            },
+            func: function(state,something,another) {
+                equal(state, 'ca');
+                equal(something, 'something');
+                equal(another, 'another');
+            }
+        });
+        router = new router();
+        ok(Backbone.history.handlers[0].route.test('ca/something/another'));
+        ok(!Backbone.history.handlers[0].route.test('dca/something/another'));
+        Backbone.history.handlers[0].callback.call(router, 'ca/something/another');
+        router.cleanup();
+        delete Controller;
+    })
+
+    test('routes with a string', function () {
+        expect(3);
+
+        var router = Viking.Router.extend({
+            routes: {
+                '': 'Controller#action',
+            }
+        });
+        router = new router();
+
+        Controller = { action: function() { ok(true); } }
+        ok(Backbone.history.handlers[0].route.test(''));
+        ok(!Backbone.history.handlers[0].route.test('dca/something/another'));
+        Backbone.history.handlers[0].callback.call(router, '');
+        router.cleanup();
+        delete Controller;
     });
-    router = new router();
-    ok(Backbone.history.handlers[0].route.test('ca/something/another'));
-    ok(!Backbone.history.handlers[0].route.test('dca/something/another'));
-    Backbone.history.handlers[0].callback.call(router, 'ca/something/another');
-    router.cleanup();
-    delete Controller;
-})
 
-test("router.start() starts Backbone.history", function() {
-    expect(1);
-    
-    var router = new Viking.Router();
-    var oldstart = Backbone.history.start
-    Backbone.history.start = function() { ok(true); }
-    router.start();
-    Backbone.history.start = oldstart;
-});
+    test("router.start() starts Backbone.history", function() {
+        expect(1);
 
-test("router.stop() stops Backbone.history", function() {
-    expect(1);
-    
-    var router = new Viking.Router();
-    var oldstop = Backbone.history.stop
-    Backbone.history.stop = function() { ok(true); }
-    router.start();
-    router.stop();
-    Backbone.history.stop = oldstop;
-    router.cleanup();
-});
+        var router = new Viking.Router();
+        var oldstart = Backbone.history.start
+        Backbone.history.start = function() { ok(true); }
+        router.start();
+        Backbone.history.start = oldstart;
+    });
 
-test('router.start() calls callbacks with the name of the route', function() {
-    expect(2);
-    
-    Controller = { action: function() { ok(true); } }
-    var r = {}
-    r[window.location.pathname.replace('/','')] = {to: 'Controller#action', name: 'root'}
-    var Router = Viking.Router.extend({ routes: r });
+    test("router.stop() stops Backbone.history", function() {
+        expect(1);
 
-    var router = new Router();
-    router.on('route:root', function() { ok(true); });
-    router.start();
+        var router = new Viking.Router();
+        var oldstop = Backbone.history.stop
+        Backbone.history.stop = function() { ok(true); }
+        router.start();
+        router.stop();
+        Backbone.history.stop = oldstop;
+        router.cleanup();
+    });
 
-    router.cleanup();
-    router.off('route:root');
-    delete Controller;
-});
+    test('router.start() calls callbacks with the name of the route', function() {
+        expect(2);
 
-test('router.navigate() calls Backbone.Router.prototype.navigate', function() {
-    expect(2);
+        Controller = { action: function() { ok(true); } }
+        var r = {}
+        r[window.location.pathname.replace('/','')] = {to: 'Controller#action', name: 'root'}
+        var Router = Viking.Router.extend({ routes: r });
 
-    var oldnavigate = Backbone.Router.prototype.navigate;
-        
-    var router = new Viking.Router();
-    Backbone.Router.prototype.navigate = function(f, a) {
-        equal(f, '/path');
-        equal(a, 'arguments');
-    };
-    router.navigate('/path', 'arguments');
-    
-    Backbone.Router.prototype.navigate = oldnavigate;
-});
+        var router = new Router();
+        router.on('route:root', function() { ok(true); });
+        router.start();
 
-test('router.navigate() strips the root_url from urls from the current domain', function() {
-    expect(2);
+        router.cleanup();
+        router.off('route:root');
+        delete Controller;
+    });
 
-    var oldnavigate = Backbone.Router.prototype.navigate;
-        
-    var router = new Viking.Router();
-    Backbone.Router.prototype.navigate = function(f, a) {
-        equal(f, '/path');
-        equal(a, 'arguments');
-    };
-    router.navigate(window.location.protocol + '//' + window.location.host + '/path', 'arguments');
-    
-    Backbone.Router.prototype.navigate = oldnavigate;
-})
+    test('router.navigate() calls Backbone.Router.prototype.navigate', function() {
+        expect(2);
+
+        var oldnavigate = Backbone.Router.prototype.navigate;
+
+        var router = new Viking.Router();
+        Backbone.Router.prototype.navigate = function(f, a) {
+            equal(f, '/path');
+            equal(a, 'arguments');
+        };
+        router.navigate('/path', 'arguments');
+
+        Backbone.Router.prototype.navigate = oldnavigate;
+    });
+
+    test('router.navigate() strips the root_url from urls from the current domain', function() {
+        expect(2);
+
+        var oldnavigate = Backbone.Router.prototype.navigate;
+
+        var router = new Viking.Router();
+        Backbone.Router.prototype.navigate = function(f, a) {
+            equal(f, '/path');
+            equal(a, 'arguments');
+        };
+        router.navigate(window.location.protocol + '//' + window.location.host + '/path', 'arguments');
+
+        Backbone.Router.prototype.navigate = oldnavigate;
+    });
+}());
