98f04967e22ec98a2ad7fa97d052112e7b453829
Julian Simioni <julian.simioni@gmail.com>
2013-12-10 14:19:56 -0800

    Whitespace

--- test/viking/model/set/hasMany.js
+++ test/viking/model/set/hasMany.js
@@ -5,13 +5,13 @@
     test("#set(key, val) coerces hasMany relations", function() {
         Ship = Viking.Model.extend({ hasMany: ['ships'] });
         ShipCollection = Viking.Collection.extend({ model: Ship });
-    
+
         var a = new Ship();
         a.set('ships', [{}, {}]);
         ok(a.get('ships') instanceof ShipCollection);
         equal(a.get('ships').length, 2);
         ok(a.get('ships').first() instanceof Ship);
-    
+
         delete Ship;
         delete ShipCollection;
     });
@@ -19,38 +19,38 @@
     test("#set({key, val}) coerces hasMany relations", function() {
         Ship = Viking.Model.extend({ hasMany: ['ships'] });
         ShipCollection = Viking.Collection.extend({ model: Ship });
-    
+
         var a = new Ship();
         a.set({ships: [{}, {}]});
         ok(a.get('ships') instanceof ShipCollection);
         equal(a.get('ships').length, 2);
         ok(a.get('ships').first() instanceof Ship);
-    
+
         delete Ship;
         delete ShipCollection;
     });
-    
-    
+
+
     test("#set(belongsToRelation, data) updates the current collection and doesn't create a new one", function() {
         expect(6);
-        
+
         Ship = Viking.Model.extend({ hasMany: ['ships'] });
         ShipCollection = Viking.Collection.extend({ model: Ship });
-    
+
         var a = new Ship();
         a.set({ships: [{id: 1}, {}, {id: 3}]});
         a.get('ships').on("add", function() { ok(true); });
         a.get('ships').get(1).on("change", function() { ok(true); });
         a.get('ships').on("remove", function() { ok(true); });
-        
+
         var oldCollection = a.get('ships');
         a.set({ships: [{id: 1, key: 'value'}, {id: 2}]});
-        
+
         strictEqual(oldCollection, a.get('ships'));
         deepEqual([1, 2], a.get('ships').map(function(s) { return s.id; }));
         a.get('ships').off();
         a.get('ships').get(1).off();
-        
+
         delete Ship;
         delete ShipCollection;
     })
