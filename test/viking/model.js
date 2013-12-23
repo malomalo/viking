62c4f6d1af938943618e56b1e47467049bee2773
Jon Bracy <jonbracy@gmail.com>
2013-12-23 12:01:00 -0800

    Added a ::where() function to Model

--- test/viking/model.js
+++ test/viking/model.js
@@ -47,4 +47,28 @@
         c.off('unselected');
     });
     
+    test("::where() returns ModelCollection without a predicate", function() {
+        Ship = Viking.Model.extend('ship');
+        ShipCollection = Viking.Collection.extend();
+        
+        var scope = Ship.where();
+        ok(scope instanceof ShipCollection);
+        strictEqual(undefined, scope.predicate);
+        
+        delete Ship;
+        delete ShipCollection;
+    });
+
+    test("::where(predicate) returns ModelCollection with a predicate set", function() {
+        Ship = Viking.Model.extend('ship');
+        ShipCollection = Viking.Collection.extend();
+        
+        var scope = Ship.where({name: 'Zoey'});
+        ok(scope instanceof ShipCollection);
+        deepEqual({name: 'Zoey'}, scope.predicate.attributes);
+        
+        delete Ship;
+        delete ShipCollection;
+    });
+    
 }());
