ccdb523cf00803858f9d13e36d73caa28e3dbf6e
James Bracy <waratuman@gmail.com>
2016-05-26 16:46:32 -0700

    Updating preamble & errors

--- viking.js
+++ viking.js
@@ -1,4 +1,4 @@
-//     Viking.js 0.9.0 (sha:0f9fd9b)
+//     Viking.js 0.9.0 (sha:4272ab3)
 //
 //     (c) 2012-2016 Jonathan Bracy, 42Floors Inc.
 //     Viking.js may be freely distributed under the MIT license.
@@ -7,21 +7,56 @@
 // Initial Setup
 // -------------
 
-// Setup the Viking namespace
-Viking = {};
-Viking.NameError = function (message) {
-  this.name = "Viking.NameError";
-  this.message = message;
+var Viking = {};
+Viking.NameError = function (message, file, lineNumber) {
+    let err = new Error();
+
+    if (err.stack) {
+        // Remove one stack level.
+        if (typeof(Components) != 'undefined') {
+            // Mozilla
+            this.stack = err.stack.substring(err.stack.indexOf('\n')+1);
+        } else if (typeof(chrome) != 'undefined' || typeof(process) != 'undefined') {
+            // Chrome / Node.js
+            this.stack = err.stack.replace(/\n[^\n]*/,'');
+        } else {
+            this.stack = err.stack;
+        }
+    }
+
+    this.name = 'Viking.NameError';
+    this.message = message === undefined ? err.message : message;
+    this.file = file === undefined ? err.file : file;
+    this.lineNumber = lineNumber === undefined ? err.lineNumber : lineNumber;
 };
 
-Viking.NameError.prototype = Error.prototype;
+Viking.NameError.prototype = Object.create(Error.prototype);
+Viking.NameError.prototype.constructor = Viking.NameError;
 
-Viking.ArgumentError = function (message) {
-  this.name = "Viking.ArgumentError";
-  this.message = message || 'Insufficient arguments';
+Viking.ArgumentError = function (message, file, lineNumber) {
+    let err = new Error();
+
+    if (err.stack) {
+        // Remove one stack level.
+        if (typeof(Components) != 'undefined') {
+            // Mozilla
+            this.stack = err.stack.substring(err.stack.indexOf('\n')+1);
+        } else if (typeof(chrome) != 'undefined' || typeof(process) != 'undefined') {
+            // Chrome / Node.js
+            this.stack = err.stack.replace(/\n[^\n]*/,'');
+        } else {
+            this.stack = err.stack;
+        }
+    }
+
+    this.name = 'Viking.ArgumentError';
+    this.message = message === undefined ? err.message : message;
+    this.file = file === undefined ? err.file : file;
+    this.lineNumber = lineNumber === undefined ? err.lineNumber : lineNumber;
 };
 
-Viking.ArgumentError.prototype = Error.prototype;
+Viking.ArgumentError.prototype = Object.create(Error.prototype);
+Viking.ArgumentError.prototype.constructor = Viking.ArgumentError;
 // Viking.Support
 // -------------
 //
