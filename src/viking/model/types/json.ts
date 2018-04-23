c737fd41651a9b72e94a3c4551053d2b8207cf29
James Bracy <waratuman@gmail.com>
2018-04-23 12:06:53 -0700

    Fixing JSON type

--- src/viking/model/types/json.ts
+++ src/viking/model/types/json.ts
@@ -3,23 +3,26 @@ import { Model } from '../../model';
 import { Name } from '../../model/name';
 
 export const JSONType = {
-
-    load: function (value: any, key: string) {
+    load(value: any, key: string) {
         if (typeof value === 'object') {
-            if(value instanceof Viking.Model) value = value.attributes;
-            var AnonModel = Model.extend({
+            if (value instanceof Model) {
+                value = value.attributes;
+            }
+
+            const AnonModel = Model.extend({
                 inheritanceAttribute: false
             });
-            var model = new AnonModel(value);
+
+            const model = new AnonModel(value);
             model.modelName = new Name((key || '').toString());
             model.baseModel = model;
-            _.each(value, function (v, k: string) {
+            _.each(value, (v, k: string) => {
                 if (_.isObject(v) && !_.isArray(v) && !_.isDate(v)) {
-                    var sub_model = JSONType.load(v, k)
-                    model.listenTo(sub_model, 'change', function () {
+                    const subModel = JSONType.load(v, k);
+                    model.listenTo(subModel, 'change', () => {
                         model.trigger('change', arguments);
                     });
-                    model.attributes[k] = sub_model;
+                    model.attributes[k] = subModel;
                 }
             });
 
@@ -28,18 +31,17 @@ export const JSONType = {
         throw new TypeError(typeof value + " can't be coerced into JSON");
     },
 
-    dump: function (value) {
+    dump(value) {
         if (value instanceof Model) {
-            var output = value.toJSON();
-            _.each(output, function (v, k) {
+            const output = value.toJSON();
+            _.each(output, (v, k) => {
                 if (v instanceof Model) {
-                    output[k] = JSONType.dump(v)
+                    output[k] = JSONType.dump(v);
                 }
-            })
+            });
             return output;
         }
 
         return value;
     }
-
 };
