3b91c24df1fb8c03eb53ea2ff53fcf1d6eee9831
Ben Ehmke <benehmke@gmail.com>
2021-02-02 14:23:06 -0600

    Relation Events (#43)
    * add relation code, needs tests
    
    * add classExtend to viking/support
    
    * Add find to collectionAssociation
    
    * add test for relation events
    
    * Misc updates
    
    Co-authored-by: Jon Bracy <jonbracy@gmail.com>

---
+++ test/record/relationTest.js
@@ -0,0 +1,46 @@
+import 'mocha';
+import * as assert from 'assert';
+import VikingRecord from 'viking/record';
+
+describe('Viking.Relation', () => {
+
+    class Model extends VikingRecord {}
+
+    describe('events', () => {
+        
+        it('add', function (done) {
+            const relation = Model.where({parent_id: 11})
+
+            relation.addEventListener('add', record => {
+                assert.equal(record.readAttribute('id'), 1);
+                done();
+            });
+
+            relation.load();
+
+            this.withRequest('GET', '/models', { params: { where: {parent_id: 11}, order: {id: 'desc'} } }, (xhr) => {
+                xhr.respond(200, {}, '[{"id": 1}]');
+            });
+        });
+        
+        it('remove', function (done) {
+            const relation = Model.where({parent_id: 11})
+
+            relation.addEventListener('remove', record => {
+                assert.equal(record.readAttribute('id'), 1);
+                done();
+            });
+            
+            relation.load().then(() => {
+                relation.addWhere({id: 2});
+                this.withRequest('GET', '/models', { params: { where: {parent_id: 11, id: 2}, order: {id: 'desc'} } }, (xhr) => {
+                    xhr.respond(200, {}, '[{"id": 2}]');
+                });
+            }).then(done, done);
+            
+            this.withRequest('GET', '/models', { params: { where: {parent_id: 11}, order: {id: 'desc'} } }, (xhr) => {
+                xhr.respond(200, {}, '[{"id": 1}, {"id": 2}]');
+            });
+        })
+    })
+})
