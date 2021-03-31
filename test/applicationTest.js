377067dd5a41f6a7ca63b408265c3066de9ceed4
Ben Ehmke <benehmke@gmail.com>
2021-03-31 12:45:04 -0500

    fix application.render to allow the result to be a promise and load the view in correct order

--- test/applicationTest.js
+++ test/applicationTest.js
@@ -2,6 +2,8 @@ import 'mocha';
 import * as assert from 'assert';
 import Application from 'viking/application';
 import View from 'viking/view';
+import Router from 'viking/router';
+import Controller from 'viking/controller';
 
 describe('Viking/Application', () => {
     
@@ -15,5 +17,39 @@ describe('Viking/Application', () => {
         app.display(MyView);
         // TODO not sure why this is failing
         // assert.equal(document.title, 'my new title');
+    });
+    
+    it('test render is promise', (done) => {
+        class MyController extends Controller {
+            show()  {
+                this.display(View).then(done, done) 
+            }
+        }
+
+        class MyRouter extends Router {
+            static routes = {
+                '/': { to: { controller: MyController, action: 'show' }}
+            };
+        }
+        
+        class MyApplication extends Application {
+            static router = MyRouter;
+            
+            async render () {
+                await new Promise(resolve => {
+                    setTimeout(() => {
+                        this.el.innerHTML = 'hello'
+                        resolve()
+                    }, 0)
+                })
+            }
+            
+            async display () {
+                console.log("**************", this.el.innerHTML);
+                assert.equal(this.el.innerHTML, 'hello');
+            }
+        }
+        
+        const app = new MyApplication();
     });
 });
