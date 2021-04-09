26be10c243a86fa82fbd3ace2ce073e52de87bcc
Jon Bracy <jonbracy@gmail.com>
2021-04-09 16:13:10 -0500

    Remove old build scripts and update ignores

--- build/imports.js
+++
@@ -1,5 +0,0 @@
-const Backbone = require("backbone");
-const _ = require("underscore");
-const msgpack = require("msgpack-lite");
-const jQuery = require("jquery");
-const strftime = require("strftime");
