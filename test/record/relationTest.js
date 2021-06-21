d80e925c70a0382300cedca847bcdb2817ae86fc
Ben Ehmke <benehmke@gmail.com>
2021-06-21 13:56:20 -0500

    fix test that was not working properly (#74)

--- test/record/relationTest.js
+++ test/record/relationTest.js
@@ -18,14 +18,18 @@ describe('Viking.Relation', () => {
                 onceFlag = true;
             });
 
-            relation.load();
+            relation.load().then(() => {
+                relation.reload().then(() => {
+                    assert.ok(true)
+                    done()
+                }, () => {
+                    assert.ok(false)
+                });
 
-            this.withRequest('GET', '/models', { params: { where: {parent_id: 11}, order: {id: 'desc'} } }, (xhr) => {
-                xhr.respond(200, {}, '[{"id": 1}]');
+                this.withRequest('GET', '/models', { params: { where: {parent_id: 11}, order: {id: 'desc'} } }, (xhr) => {
+                    xhr.respond(200, {}, '[{"id": 1}]');
+                });
             });
-            
-            relation.reload().then(() => done(), done);
-            
 
             this.withRequest('GET', '/models', { params: { where: {parent_id: 11}, order: {id: 'desc'} } }, (xhr) => {
                 xhr.respond(200, {}, '[{"id": 1}]');
@@ -41,10 +45,11 @@ describe('Viking.Relation', () => {
             
             relation.load().then(() => {
                 relation.addWhere({id: 2});
+                relation.load().then(() => done(), done)
                 this.withRequest('GET', '/models', { params: { where: {parent_id: 11, id: 2}, order: {id: 'desc'} } }, (xhr) => {
                     xhr.respond(200, {}, '[{"id": 2}]');
                 });
-            }).then(done, done);
+            });
             
             this.withRequest('GET', '/models', { params: { where: {parent_id: 11}, order: {id: 'desc'} } }, (xhr) => {
                 xhr.respond(200, {}, '[{"id": 1}, {"id": 2}]');
@@ -58,7 +63,7 @@ describe('Viking.Relation', () => {
                 assert.deepEqual(records.map(x => x.readAttribute('id')), [1]);
             });
             
-            relation.load().then(x => done(), done);
+            relation.load().then(() => done(), done);
 
             this.withRequest('GET', '/models', { params: { where: {parent_id: 11}, order: {id: 'desc'} } }, (xhr) => {
                 xhr.respond(200, {}, '[{"id": 1}]');
