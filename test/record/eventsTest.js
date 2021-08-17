7c78fee20228fdfab4ada7c9b581c3dc32b2aec4
Ben Ehmke <benehmke@gmail.com>
2021-08-17 13:27:22 -0700

    add error callback to record (#78)
    * add error callback to record
    
    * Add comment for invalid event
    
    * flip else so only 400 responses are invalid and everything else is error
    
    * Add invalid event test for record
    
    * Scope invalid to just 400 exactly

--- test/record/eventsTest.js
+++ test/record/eventsTest.js
@@ -83,5 +83,41 @@ describe('Viking.Record#events', () => {
             xhr.respond(201, {}, '{"id": 1, "name": "Rod Kimbal", "imdb_id": "999"}');
         });
     });
+    
+    it('error', function(done) {
+
+      const actor = new Actor({name: 'Fred'});
+      
+      actor.addEventListener('error', (response) => {
+          assert.equal("something went wrong", response)
+          done()
+      })
+      
+      actor.save()
+  
+      this.withRequest('POST', '/actors', { body: {actor: {name: 'Fred'} } }, (xhr) => {
+          xhr.respond(500, {}, 'something went wrong');
+      });
+
+    });
+    
+    it('invalid', function(done) {
+
+      const actor = new Actor({name: 'Fred'});
+      
+      actor.addEventListener('invalid', () => {
+          assert.deepEqual(actor.errors, {name: ["can only be Jimmy"]})
+          done()
+      })
+      
+      actor.save()
+  
+      this.withRequest('POST', '/actors', { body: {actor: {name: 'Fred'} } }, (xhr) => {
+          xhr.respond(400, {
+              'Content-Type': 'application/json'
+          }, '{"id": null, "name": "Fred", "errors": {"name": ["can only be Jimmy"]}}');
+      });
+
+    });
 
 });
