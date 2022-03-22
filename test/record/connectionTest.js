a8de7abb361274a055f48edab7950dc2a27b39ce
Ben Ehmke <benehmke@gmail.com>
2022-03-22 12:10:55 -0500

    add complete callback for sendRequest and test (#96)

--- test/record/connectionTest.js
+++ test/record/connectionTest.js
@@ -15,6 +15,124 @@ describe('Viking.Record', () => {
                 assert.equal(xhr.requestHeaders['X-CSRF-Token'], "ETZaIMiq");
             });
         });
+        
+        describe('callbacks', () => {        
+            it('success', function (done) {
+                let connection = new Connection('http://example.com');
+                let counter = 0;
+                
+                connection.get('/', {
+                    success: response => {
+                        counter++;
+                    }
+                });
+                this.withRequest('GET', '/', {}, (xhr) => xhr.respond(400, {}, '{"foo": "bar"}'));
+                
+                connection.get('/', {
+                    success: response => {
+                        counter++;
+                    }
+                });
+                this.withRequest('GET', '/', {}, (xhr) => xhr.respond(500, {}, '{"foo": "bar"}'));
+                
+                connection.get('/', {
+                    success: response => {
+                        assert.equal(response.foo, 'bar');
+                        assert.equal(counter, 0);
+                        done()
+                    }
+                });
+                this.withRequest('GET', '/', {}, (xhr) => xhr.respond(201, {}, '{"foo": "bar"}'));
+            });
+            
+            it('complete', function (done) {
+                let connection = new Connection('http://example.com');
+                let counter = 0;
+                
+                connection.get('/', {
+                    complete: response => {
+                        assert.equal(response, '{"foo": "bar"}');
+                        counter++;
+                    }
+                });            
+                this.withRequest('GET', '/', {}, (xhr) => xhr.respond(201, {}, '{"foo": "bar"}'));
+                
+                connection.get('/', {
+                    complete: response => {
+                        assert.equal(response, '{"foo": "bar"}');
+                        counter ++;
+                    }
+                });            
+                this.withRequest('GET', '/', {}, (xhr) => xhr.respond(400, {}, '{"foo": "bar"}'));
+                
+                connection.get('/', {
+                    complete: response => {
+                        assert.equal(response, '{"foo": "bar"}');
+                        assert.equal(counter, 2)
+                        done()
+                    }
+                });            
+                this.withRequest('GET', '/', {}, (xhr) => xhr.respond(500, {}, '{"foo": "bar"}'));
+            });
+            
+            it('invalid', function (done) {
+                let connection = new Connection('http://example.com');
+                let counter = 0;
+                
+                connection.get('/', {
+                    invalid: request => {
+                        counter++;
+                    }
+                });
+                this.withRequest('GET', '/', {}, (xhr) => xhr.respond(201, {}, '{"foo": "bar"}'));
+                
+                connection.get('/', {
+                    invalid: request => {
+                        counter++;
+                    }
+                });
+                this.withRequest('GET', '/', {}, (xhr) => xhr.respond(500, {}, '{"foo": "bar"}'));
+                
+                connection.get('/', {
+                    invalid: request => {
+                        assert.equal(request.response, '{"foo": "bar"}');
+                        assert.equal(counter, 0);
+                        done()
+                    }
+                });
+                this.withRequest('GET', '/', {}, (xhr) => xhr.respond(400, {}, '{"foo": "bar"}'));
+            });
+            
+            it('error', function (done) {
+                let connection = new Connection('http://example.com');
+                let counter = 0;
+                
+                connection.get('/', {
+                    error: response => {
+                        counter++;
+                    }
+                });
+                this.withRequest('GET', '/', {}, (xhr) => xhr.respond(201, {}, '{"foo": "bar"}'));
+                
+                connection.get('/', {
+                    error: response => {
+                        counter++;
+                    }
+                });
+                this.withRequest('GET', '/', {}, (xhr) => xhr.respond(400, {}, '{"foo": "bar"}'));
+                
+                connection.get('/', {
+                    error: response => {
+                        assert.equal(response, '{"foo": "bar"}');
+                        assert.equal(counter, 0);
+                        done()
+                    }
+                });
+                this.withRequest('GET', '/', {}, (xhr) => xhr.respond(500, {}, '{"foo": "bar"}'));
+            });
+            
+            
+        })
 
     });
 });
