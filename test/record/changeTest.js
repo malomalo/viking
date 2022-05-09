33fe93e60a26b385cc1ef07aee215eb5f4c3b5e2
Ben Ehmke <benehmke@gmail.com>
2022-05-09 16:23:21 -0500

    add more json tests

--- test/record/changeTest.js
+++ test/record/changeTest.js
@@ -8,13 +8,14 @@ describe('Viking.Record', () => {
             id: {type: "integer"},
             name: {type: 'string'},
             age: {type: "integer"},
-            union: {type: "boolean"}
+            union: {type: "boolean"},
+            preferences: {type: "json", default: {}}
         }
     }
 
     it('#changes()', () => {
         let model = new Actor({name: 'Rod Kimbal', age: 30});
-        assert.deepEqual(model.changes(), { name: [null, 'Rod Kimbal'], age: [null, 30] });
+        assert.deepEqual(model.changes(), { name: [null, 'Rod Kimbal'], age: [null, 30], preferences: [null, {}] });
 
         model.persist();
         assert.deepEqual(model.changes(), {});
@@ -29,6 +30,49 @@ describe('Viking.Record', () => {
         assert.deepEqual(model.changes(), {
             name: ['Rod Kimbal', 'Andy Sanberg'],
             age: [30, 36]
+        });
+        
+        model.preferences = {
+            green_room: true
+        }
+        assert.deepEqual(model.changes(), {
+            name: ['Rod Kimbal', 'Andy Sanberg'],
+            age: [30, 36],
+            preferences: [
+                {},
+                {green_room: true}
+            ]
+        });
+        
+        model.persist();
+        model.preferences = {green_room: true, red_room: false};
+        assert.deepEqual(model.changes(), {
+            preferences: [
+                {green_room: true},
+                {green_room: true, red_room: false}
+            ]
+        });
+        
+        model.persist();
+        model.preferences = {green_room: true, red_room: true};
+        assert.deepEqual(model.changes(), {
+            preferences: [
+                {green_room: true, red_room: false},
+                {green_room: true, red_room: true}
+            ]
+        });
+        
+        model.preferences = [{foo: 'bar'}]
+        model.persist()
+        model.preferences = [{
+            foo: 'echo'
+        }]
+        
+        assert.deepEqual(model.changes(), {
+            preferences: [
+                [{foo: 'bar'}],
+                [{foo: 'echo'}],
+            ]
         });
     });
     
@@ -46,7 +90,7 @@ describe('Viking.Record', () => {
         
         let actor = new Actor()
         actor.name = "Rod Kimbal";
-        assert.deepEqual(actor.changedAttributes(), ['name']);
+        assert.deepEqual(actor.changedAttributes(), ['preferences', 'name']);
     });
     
     it('#hasChanged()', () => {
