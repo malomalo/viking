b89e8366694f5dfd748c54dc4720f1538020daca
Jon Bracy <jonbracy@gmail.com>
2022-08-12 11:10:58 -0500

    Add Collection Association Coalescing (#105)

---
+++ test/record/relation/loadTest.js
@@ -0,0 +1,30 @@
+import 'mocha';
+import * as assert from 'assert';
+import VikingRecord from 'viking/record';
+
+describe('Viking.Relation', () => {
+
+    class Model extends VikingRecord {}
+
+    describe('load', () => {
+        
+        it('coalesces loads when a inflight load is happening', function (done) {
+            const relation = Model.where({parent_id: 11})
+            const callback_order = [];
+            
+            const p1 = relation.load().then(() => { callback_order.push(1); });
+            const p2 = relation.load().then(() => { callback_order.push(2); });
+
+            this.withRequest('GET', '/models', { params: { where: {parent_id: 11}, order: {id: 'desc'} } }, (xhr) => {
+                xhr.respond(200, {}, '[{"id": 1}]');
+            });
+            
+            Promise.all([p1,p2]).then(() => {
+                assert.deepEqual(callback_order, [1,2]);
+                done();
+            });
+        });
+
+    })
+    
+})
