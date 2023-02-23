c86f2e19e5ca41870f0f1e7c92cc775949686f02
Jon Bracy <jonbracy@gmail.com>
2023-02-23 14:38:18 -0600

    Fix rewhere

--- test/record/classProperties/rewhereTest.js
+++ test/record/classProperties/rewhereTest.js
@@ -10,7 +10,7 @@ describe('Viking.Record::rewhere', () => {
     class Ship extends VikingRecord { }
     
     it('::where(predicate)', function(done) {
-        let relation = Ship.where({id: [999]});
+        let relation = Ship.where({id: [999], type: 'Big'});
         
         relation.rewhere({id: [111, 888]}).load().then((models) => {
             assert.equal(models.length, 1);
@@ -18,7 +18,7 @@ describe('Viking.Record::rewhere', () => {
             assert.equal(models[0].readAttribute('id'), 111);
         }).then(() => { done() }, done);
         
-        this.withRequest('GET', '/ships', { params: { where: {id: [111, 888]}, order: {id: 'desc'} } }, (xhr) => {
+        this.withRequest('GET', '/ships', { params: { where: {type: 'Big', id: [111, 888]}, order: {id: 'desc'} } }, (xhr) => {
             xhr.respond(200, {}, '[{"id": 111}]');
         });
     });
