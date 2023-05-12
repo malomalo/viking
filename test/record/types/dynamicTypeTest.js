bd328146f8585f0a6ff54949849c95f165026ef0
Ben Ehmke <benehmke@gmail.com>
2023-05-12 15:01:56 -0500

    allow schema types to be functions to dynamically set type (#113)

---
+++ test/record/types/dynamicTypeTest.js
@@ -0,0 +1,60 @@
+import 'mocha';
+import * as assert from 'assert';
+import Record from 'viking/record';
+
+describe('Viking.Record.Types', () => {
+    
+    describe('custom', () => {
+    
+        class Foo extends Record {
+            static schema = {
+                value: {type: (r, changes={}) => {
+                    return changes.type || r.readAttribute('type')
+                }}
+            }
+        }
+
+        it("init", () => {
+            let record = new Foo({
+                type: 'integer',
+                value: 9
+            })
+            assert.deepEqual(9, record.value);
+            
+            record = new Foo({
+                value: 9,
+                type: 'string'
+            })
+            assert.deepEqual('9', record.value);
+            
+            record = new Foo({
+                type: 'boolean',
+                value: 9
+            })
+            assert.deepEqual(true, record.value);
+        });
+        
+        it("setting attribute", () => {
+            const record = new Foo({
+                value: 9,
+                type: 'string'
+            })
+
+            record.value = 3
+            assert.deepEqual('3', record.value);
+        });
+
+        it("asJSON", () => {
+            const record = new Foo({
+                value: 9,
+                type: 'string'
+            })
+
+            assert.deepEqual({
+                type: 'string',
+                value: '9'
+            }, record.asJSON())
+        });
+
+    });
+});
