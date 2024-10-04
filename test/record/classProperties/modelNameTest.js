b7c46e70b183067eb4f9cc536a638b97972ddd45
Ben Ehmke <benehmke@gmail.com>
2024-10-04 14:29:03 -0500

    fix modelName being set by basemodel

---
+++ test/record/classProperties/modelNameTest.js
@@ -0,0 +1,26 @@
+import 'mocha';
+import * as assert from 'assert';
+import Model from 'viking/record';
+
+describe('Viking.Record::modelName', () => {
+
+    it('Object.prototype properties are overridden by attributes', () => {
+        class Player extends Model {}
+        class Team extends Model {}
+        class Conference extends Model {}
+        class League extends Model {}
+        
+        Model.modelName()
+        
+        assert.deepEqual(
+            ['players', 'teams', 'conferences', 'leagues'],
+            [
+                Player.modelName().routeKey,
+                Team.modelName().routeKey,
+                Conference.modelName().routeKey,
+                League.modelName().routeKey
+            ]
+        )
+    });
+});
+
