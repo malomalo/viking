3ac75890e1e7b6d428acfbb1ca8cbbb07605d12c
Ben Ehmke <benehmke@gmail.com>
2025-04-17 15:38:53 -0500

    add setting of inheritanceAttribute to Record.constructor

--- test/record/instanceProperties/inheritanceAttributeTest.js
+++ test/record/instanceProperties/inheritanceAttributeTest.js
@@ -5,10 +5,19 @@ import VikingRecord from 'viking/record';
 describe('Viking.Record#inheritanceAttribute', () => {
 
     it("default to `type`", () => {
-        class Ship extends VikingRecord { }
+        class Ship extends VikingRecord {
+            static schema = {
+                type: {type: 'string'}
+            }
+        }
         let ship = new Ship();
 
         assert.equal('type', ship.inheritanceAttribute);
+
+        class Battleship extends Ship { }
+        let battleship = new Battleship();
+
+        assert.equal('Battleship', battleship.readAttribute('type'));
     });
 
     it("override", () => {
