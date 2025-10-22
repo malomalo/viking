da85e33c2efe52fc0c0306dd8c12036f9a20c4d9
Jon Bracy <jonbracy@gmail.com>
2025-10-22 09:19:40 -0500

    testSetup just needs to setup jsdom now

--- test/testSetup.js
+++ test/testSetup.js
@@ -1,11 +1,4 @@
-import path from 'path';
-import module from 'module';
 import jsdom from 'jsdom-global';
-
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 jsdom('', {
     url: "http://example.com/",
@@ -15,6 +8,3 @@ jsdom('', {
         window.scrollTo = function () {}
     }
 });
-
-process.env.NODE_PATH = path.resolve(__dirname + '/../lib:') + process.env.NODE_PATH;
-module._initPaths();
